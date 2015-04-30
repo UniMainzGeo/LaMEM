ba68419583875be63242bc8abda396c7d6a8b1c1
Tobias Baumann <baumann@uni-mainz.de>
2015-04-30 12:34:05 +0000

    added options to the archive script. Call it as follows
    # 1.) ./archiveTS.sh                    # create archives of Timestep folders
    # 2.) ./archiveTS.sh -deldir        # create archives of Timestep folders & delete folders after creating the respective archives
    # 3.) ./archiveTS.sh -untar         # extract the Timestep-archives
    
    git-svn-id: svn+ssh://gaia.geo.uni-mainz.de/local/home/lkausb/svn/LaMEM/trunk@5871 26174531-3d5a-4d31-aba0-7a6658d032cb

--- utils/archiveTS.sh
+++ utils/archiveTS.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
 # Create archives for all timesteps 
-# ./archiveTS.sh
+#
+# --> Use it as follows
+#
+# 1.) ./archiveTS.sh
+# 2.) ./archiveTS.sh -deldir
+# 3.) ./archiveTS.sh -untar
+#
+# two possible options: 
+# -deldir # removes directory after creating an archive
+# -untar  # extract Timestep-archives
+#
 #
 # Tobias Baumann, Mainz 2015
 
 
-for k in Timestep_*_*[0-9]
-do
-	tar -cf "${k}.tar" ${k}
-done
+if [ "-untar" = "$1" ]
+then
+	for k in Timestep_*_*[0-9].tar
+	do
+		echo "> extract ${k}"
+		tar -xf ${k}
+	done
+else
+	for k in Timestep_*_*[0-9]
+	do
+		echo "${k}"
+		tar -cf "${k}.tar" ${k}
+		if [ "-deldir" = "$1" ]
+		then
+			echo "> delete ${k}"
+			rm -rf ${k}
+		fi
+	done
+fi
