5f8b0f42f0e57d5bbe1d1b695dbb777e81c42f64
lapopov <popov@uni-mainz.de>
2015-12-03 01:29:44 +0100

    Picard Jacobian Matrix Free (complete Jacobian is under construction)

---
+++ src/matFree.c
@@ -0,0 +1,288 @@
+/*@ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ **
+ **    Copyright (c) 2011-2015, JGU Mainz, Anton Popov, Boris Kaus
+ **    All rights reserved.
+ **
+ **    This software was developed at:
+ **
+ **         Institute of Geosciences
+ **         Johannes-Gutenberg University, Mainz
+ **         Johann-Joachim-Becherweg 21
+ **         55128 Mainz, Germany
+ **
+ **    project:    LaMEM
+ **    filename:   matFree.c
+ **
+ **    LaMEM is free software: you can redistribute it and/or modify
+ **    it under the terms of the GNU General Public License as published
+ **    by the Free Software Foundation, version 3 of the License.
+ **
+ **    LaMEM is distributed in the hope that it will be useful,
+ **    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ **    See the GNU General Public License for more details.
+ **
+ **    You should have received a copy of the GNU General Public License
+ **    along with LaMEM. If not, see <http://www.gnu.org/licenses/>.
+ **
+ **
+ **    Contact:
+ **        Boris Kaus       [kaus@uni-mainz.de]
+ **        Anton Popov      [popov@uni-mainz.de]
+ **
+ **
+ **    Main development team:
+ **         Anton Popov      [popov@uni-mainz.de]
+ **         Boris Kaus       [kaus@uni-mainz.de]
+ **         Tobias Baumann
+ **         Adina Pusok
+ **         Arthur Bauville
+ **
+ ** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ @*/
+//---------------------------------------------------------------------------
+//...............   MATRIX-FREE JACOBIAN AND PRECONDITIONER  ................
+//---------------------------------------------------------------------------
+
+#include "LaMEM.h"
+#include "fdstag.h"
+#include "solVar.h"
+#include "scaling.h"
+#include "tssolve.h"
+#include "bc.h"
+#include "JacRes.h"
+#include "matFree.h"
+//---------------------------------------------------------------------------
+#undef __FUNCT__
+#define __FUNCT__ "JacApplyPicard"
+PetscErrorCode JacApplyPicard(Mat A, Vec x, Vec y)
+{
+	JacRes *jr;
+
+	PetscErrorCode ierr;
+	PetscFunctionBegin;
+
+	// access context
+	ierr = MatShellGetContext(A, (void**)&jr); CHKERRQ(ierr);
+
+	// copy solution from global to local vectors, enforce boundary constraints
+	ierr = JacResCopySol(jr, x); CHKERRQ(ierr);
+
+	ierr = JacResPicardMatFree(jr); CHKERRQ(ierr);
+
+	// copy residuals to global vector
+	ierr = JacResCopyRes(jr, y); CHKERRQ(ierr);
+
+	PetscFunctionReturn(0);
+}
+//-----------------------------------------------------------------------------
+#undef __FUNCT__
+#define __FUNCT__ "JacResPicardMatFree"
+PetscErrorCode JacResPicardMatFree(JacRes *jr)
+{
+	FDSTAG     *fs;
+	PetscInt    i, j, k, nx, ny, nz, sx, sy, sz, iter;
+	PetscScalar dx, dy, dz, tx, ty, tz;
+	PetscScalar bdx, fdx, bdy, fdy, bdz, fdz;
+	PetscScalar sxx, syy, szz, sxy, sxz, syz;
+	PetscScalar dxx, dyy, dzz, dvxdy, dvydx, dvxdz, dvzdx, dvydz, dvzdy;
+	PetscScalar eta, theta, tr, rho, IKdt, pc, dt, fssa, *grav;
+	PetscScalar ***fx,  ***fy,  ***fz, ***vx,  ***vy,  ***vz, ***gc, ***p;
+
+	PetscErrorCode ierr;
+	PetscFunctionBegin;
+
+	fs   = jr->fs;
+	dt   = jr->ts.dt; // time step
+	fssa = jr->FSSA;  // density gradient penalty parameter
+    grav = jr->grav;  // gravity acceleration
+
+    // clear local residual vectors
+	ierr = VecZeroEntries(jr->lfx); CHKERRQ(ierr);
+	ierr = VecZeroEntries(jr->lfy); CHKERRQ(ierr);
+	ierr = VecZeroEntries(jr->lfz); CHKERRQ(ierr);
+
+	// access work vectors
+	ierr = DMDAVecGetArray(fs->DA_CEN, jr->gc,   &gc);  CHKERRQ(ierr);
+	ierr = DMDAVecGetArray(fs->DA_CEN, jr->lp,   &p);   CHKERRQ(ierr);
+	ierr = DMDAVecGetArray(fs->DA_X,   jr->lfx,  &fx);  CHKERRQ(ierr);
+	ierr = DMDAVecGetArray(fs->DA_Y,   jr->lfy,  &fy);  CHKERRQ(ierr);
+	ierr = DMDAVecGetArray(fs->DA_Z,   jr->lfz,  &fz);  CHKERRQ(ierr);
+	ierr = DMDAVecGetArray(fs->DA_X,   jr->lvx,  &vx);  CHKERRQ(ierr);
+	ierr = DMDAVecGetArray(fs->DA_Y,   jr->lvy,  &vy);  CHKERRQ(ierr);
+	ierr = DMDAVecGetArray(fs->DA_Z,   jr->lvz,  &vz);  CHKERRQ(ierr);
+
+	//-------------------------------
+	// central points
+	//-------------------------------
+	iter = 0;
+	ierr = DMDAGetCorners(fs->DA_CEN, &sx, &sy, &sz, &nx, &ny, &nz); CHKERRQ(ierr);
+
+	START_STD_LOOP
+	{
+		// get density, shear & inverse bulk viscosities
+		eta  = jr->svCell[iter].svDev.eta;
+		IKdt = jr->svCell[iter].svBulk.IKdt;
+		rho  = jr->svCell[iter].svBulk.rho;
+
+		// get mesh steps
+		dx = SIZE_CELL(i, sx, fs->dsx);
+		dy = SIZE_CELL(j, sy, fs->dsy);
+		dz = SIZE_CELL(k, sz, fs->dsz);
+
+		// compute velocity gradients
+		dxx = (vx[k][j][i+1] - vx[k][j][i])/dx;
+		dyy = (vy[k][j+1][i] - vy[k][j][i])/dy;
+		dzz = (vz[k+1][j][i] - vz[k][j][i])/dz;
+
+		// compute & store volumetric strain rate
+		theta = dxx + dyy + dzz;
+
+		// compute & store total deviatoric strain rates
+		tr  = theta/3.0;
+		dxx -= tr;
+		dyy -= tr;
+		dzz -= tr;
+
+		// access current pressure
+		pc = p[k][j][i];
+
+		// compute total Cauchy stresses
+		sxx = 2.0*eta*dxx - pc;
+		syy = 2.0*eta*dyy - pc;
+		szz = 2.0*eta*dzz - pc;
+
+		// compute stabilization terms (lumped approximation)
+		tx = -fssa*dt*rho*grav[0];
+		ty = -fssa*dt*rho*grav[1];
+		tz = -fssa*dt*rho*grav[2];
+
+		// get mesh steps for the backward and forward derivatives
+		bdx = SIZE_NODE(i, sx, fs->dsx);   fdx = SIZE_NODE(i+1, sx, fs->dsx);
+		bdy = SIZE_NODE(j, sy, fs->dsy);   fdy = SIZE_NODE(j+1, sy, fs->dsy);
+		bdz = SIZE_NODE(k, sz, fs->dsz);   fdz = SIZE_NODE(k+1, sz, fs->dsz);
+
+		// momentum
+		fx[k][j][i] -= (sxx + vx[k][j][i]*tx)/bdx;   fx[k][j][i+1] += (sxx + vx[k][j][i+1]*tx)/fdx;
+		fy[k][j][i] -= (syy + vy[k][j][i]*ty)/bdy;   fy[k][j+1][i] += (syy + vy[k][j+1][i]*ty)/fdy;
+		fz[k][j][i] -= (szz + vz[k][j][i]*tz)/bdz;   fz[k+1][j][i] += (szz + vz[k+1][j][i]*tz)/fdz;
+
+		// mass
+		gc[k][j][i] = -IKdt*pc - theta;
+	}
+	END_STD_LOOP
+
+	//-------------------------------
+	// xy edge points
+	//-------------------------------
+	iter = 0;
+	ierr = DMDAGetCorners(fs->DA_XY, &sx, &sy, &sz, &nx, &ny, &nz); CHKERRQ(ierr);
+
+	START_STD_LOOP
+	{
+		// get effective viscosity
+		eta = jr->svXZEdge[iter++].svDev.eta;
+
+		// get mesh steps
+		dx = SIZE_NODE(i, sx, fs->dsx);
+		dy = SIZE_NODE(j, sy, fs->dsy);
+
+		// compute velocity gradients
+		dvxdy = (vx[k][j][i] - vx[k][j-1][i])/dy;
+		dvydx = (vy[k][j][i] - vy[k][j][i-1])/dx;
+
+		// compute stress
+		sxy = eta*(dvxdy + dvydx);
+
+		// get mesh steps for the backward and forward derivatives
+		bdx = SIZE_CELL(i-1, sx, fs->dsx);   fdx = SIZE_CELL(i, sx, fs->dsx);
+		bdy = SIZE_CELL(j-1, sy, fs->dsy);   fdy = SIZE_CELL(j, sy, fs->dsy);
+
+		// momentum
+		fx[k][j-1][i] -= sxy/bdy;   fx[k][j][i] += sxy/fdy;
+		fy[k][j][i-1] -= sxy/bdx;   fy[k][j][i] += sxy/fdx;
+
+	}
+	END_STD_LOOP
+
+	//-------------------------------
+	// xz edge points
+	//-------------------------------
+	iter = 0;
+	ierr = DMDAGetCorners(fs->DA_XZ, &sx, &sy, &sz, &nx, &ny, &nz); CHKERRQ(ierr);
+
+	START_STD_LOOP
+	{
+		// get effective viscosity
+		eta = jr->svXZEdge[iter++].svDev.eta;
+
+		// get mesh steps
+		dx = SIZE_NODE(i, sx, fs->dsx);
+		dz = SIZE_NODE(k, sz, fs->dsz);
+
+		// compute velocity gradients
+		dvxdz = (vx[k][j][i] - vx[k-1][j][i])/dz;
+		dvzdx = (vz[k][j][i] - vz[k][j][i-1])/dx;
+
+		// compute stress
+        sxz = eta*(dvxdz + dvzdx);
+
+		// get mesh steps for the backward and forward derivatives
+		bdx = SIZE_CELL(i-1, sx, fs->dsx);   fdx = SIZE_CELL(i, sx, fs->dsx);
+		bdz = SIZE_CELL(k-1, sz, fs->dsz);   fdz = SIZE_CELL(k, sz, fs->dsz);
+
+		// momentum
+		fx[k-1][j][i] -= sxz/bdz;   fx[k][j][i] += sxz/fdz;
+		fz[k][j][i-1] -= sxz/bdx;   fz[k][j][i] += sxz/fdx;
+	}
+	END_STD_LOOP
+
+	//-------------------------------
+	// yz edge points
+	//-------------------------------
+	iter = 0;
+	ierr = DMDAGetCorners(fs->DA_YZ, &sx, &sy, &sz, &nx, &ny, &nz); CHKERRQ(ierr);
+
+	START_STD_LOOP
+	{
+		// get effective viscosity
+		eta = jr->svYZEdge[iter++].svDev.eta;
+
+		// get mesh steps
+		dy = SIZE_NODE(j, sy, fs->dsy);
+		dz = SIZE_NODE(k, sz, fs->dsz);
+
+		// compute velocity gradients
+		dvydz = (vy[k][j][i] - vy[k-1][j][i])/dz;
+		dvzdy = (vz[k][j][i] - vz[k][j-1][i])/dy;
+
+		// compute stress
+		syz = eta*(dvydz + dvzdy);
+
+		// get mesh steps for the backward and forward derivatives
+		bdy = SIZE_CELL(j-1, sy, fs->dsy);   fdy = SIZE_CELL(j, sy, fs->dsy);
+		bdz = SIZE_CELL(k-1, sz, fs->dsz);   fdz = SIZE_CELL(k, sz, fs->dsz);
+
+		// update momentum residuals
+		fy[k-1][j][i] -= syz/bdz;   fy[k][j][i] += syz/fdz;
+		fz[k][j-1][i] -= syz/bdy;   fz[k][j][i] += syz/fdy;
+	}
+	END_STD_LOOP
+
+	// restore vectors
+	ierr = DMDAVecRestoreArray(fs->DA_CEN, jr->gc,   &gc);  CHKERRQ(ierr);
+	ierr = DMDAVecRestoreArray(fs->DA_CEN, jr->lp,   &p);   CHKERRQ(ierr);
+	ierr = DMDAVecRestoreArray(fs->DA_X,   jr->lfx,  &fx);  CHKERRQ(ierr);
+	ierr = DMDAVecRestoreArray(fs->DA_Y,   jr->lfy,  &fy);  CHKERRQ(ierr);
+	ierr = DMDAVecRestoreArray(fs->DA_Z,   jr->lfz,  &fz);  CHKERRQ(ierr);
+	ierr = DMDAVecRestoreArray(fs->DA_X,   jr->lvx,  &vx);  CHKERRQ(ierr);
+	ierr = DMDAVecRestoreArray(fs->DA_Y,   jr->lvy,  &vy);  CHKERRQ(ierr);
+	ierr = DMDAVecRestoreArray(fs->DA_Z,   jr->lvz,  &vz);  CHKERRQ(ierr);
+
+	// assemble global residuals from local contributions
+	LOCAL_TO_GLOBAL(fs->DA_X, jr->lfx, jr->gfx)
+	LOCAL_TO_GLOBAL(fs->DA_Y, jr->lfy, jr->gfy)
+	LOCAL_TO_GLOBAL(fs->DA_Z, jr->lfz, jr->gfz)
+
+	PetscFunctionReturn(0);
+}
+//---------------------------------------------------------------------------
