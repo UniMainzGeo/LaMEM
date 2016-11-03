4b81181484ceb8599f4bec6fb68f582b5e631295
Boris Kaus <kaus@uni-mainz.de>
2016-11-03 22:32:10 +0100

    bugfixes (code did not actually run), plus few values for various rock types

--- input_models/ExplicitWavePropagation/ElasticPropertiesConversion.m
+++ input_models/ExplicitWavePropagation/ElasticPropertiesConversion.m
@@ -1,14 +1,35 @@
-% Elastic properties
+% Input:    density & seismic wave velocuty
+% Output:   Elastic properties
+clear
 
-vp  = 5000          % P wave velocity
-vs  = vp/sqrt(3)    % S wave velocity
+
+% Salt properties
 rho = 2200          % density
-
-mu    = vs^2*rho;                       % shear, G
-lamda = vp^2*rho-2*G;
-K     = density*(vp^2 - (4/3)*vs^2)     % bulk
+vp  = 4500          % P wave velocity [m/s]
+vs  = vp/sqrt(3)    % S wave velocity
 
 
+% Basement (granite) properties
+rho = 2900          % density
+vp  = 6000          % P wave velocity [m/s]
+vs  = vp/sqrt(3)    % S wave velocity
 
-vp = sqrt((K+(4/3)*G)/density)  % P wave velocity
-vs = sqrt(mu/rho)               % S wave velocity
+
+% Sediment1 (sandstone) properties
+rho = 2900          % density
+vp  = 5000          % P wave velocity [m/s]
+vs  = vp/sqrt(3)    % S wave velocity
+
+% Sediment2 (sandstone) properties
+rho = 2900          % density
+vp  = 4000          % P wave velocity [m/s]
+vs  = vp/sqrt(3)    % S wave velocity
+
+
+G       = vs^2*rho                      % shear, G
+lamda   = vp^2*rho-2*G;
+K       = rho*(vp^2 - (4/3)*vs^2)     % bulk
+
+
+vp = sqrt((K+(4/3)*G)/rho);  % P wave velocity
+vs = sqrt(G/rho);               % S wave velocity
