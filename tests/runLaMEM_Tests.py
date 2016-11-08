9207a08c25549c039d9fc0cd82e96fc39976cbb7
Boris Kaus <kaus@uni-mainz.de>
2016-11-08 23:46:38 +0100

    Added a new test, which shows how to tests the workflow that is done to generate an input setup from within MATLAB on 1 and multiple cores.

--- tests/runLaMEM_Tests.py
+++ tests/runLaMEM_Tests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os,sys
+import subprocess
 sys.path.append(os.path.join(os.environ['PWD'], 'pyTestHarness'))
 
 import argparse
@@ -7,13 +8,28 @@ import argparse
 import pyTestHarness.unittest as pth
 import pyTestHarness.launch as launch
 
+def cmd_exists(cmd):
+    return subprocess.call("type " + cmd, shell=True, 
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
+
+#os.system('$MATLAB -nojvm -r "disp(1+1); exit"')
 
 # Import separate tests 
 sys.path.append(os.path.join(os.environ['PWD'], 't1_FB1_Direct'))
 sys.path.append(os.path.join(os.environ['PWD'], 't2_FB2_MG'))
 
+# add matlab-tests if matlab is available as ENVIRONMENTAL variable MATLAB
+if cmd_exists("$MATLAB") == True:
+    sys.path.append(os.path.join(os.environ['PWD'], 't3_SubductionMATLABinput'))
+else:
+    print('MATLAB tests cannot be executed, as the environmental variable $MATLAB cannot be set')
+     
 import test_1_FB1 as FB1
 import test_2_FB2 as FB2
+
+if cmd_exists("$MATLAB") == True:
+  import test_3_Subduction1 as Sub1 # import test that requires MATLAB
+
 
 def run_unittests_example1():
   os.environ['PYTHONUNBUFFERED'] = str('1')
@@ -21,28 +37,31 @@ def run_unittests_example1():
   if os.path.isdir('output') == False:
     os.mkdir('output')
 
-  # Register all tests
+  # Register all non-MATLAB tests
   registeredTests = [ FB1.test_a(), FB1.test_b(), FB1.test_c(), FB1.test_d(),
-                      FB2.test_a() ]
+                      FB2.test_a()];
+  
+  # Add matlab tests (There should be a better way to do this for a range of files at the same time)
+  if cmd_exists("$MATLAB") == True:   
+    registeredTests.append(Sub1.test_a());
+    registeredTests.append(Sub1.test_b());
+    registeredTests.append(Sub1.test_c());
 
   # Force output to written somewhere else, can be invoked using -o <path>
   for test in registeredTests:
     test.setOutputPath('output')
 
-  # Clean current directory
-  os.system('make clean')
-  
   # Build optimized and debug versions of LaMEM
   os.system('cd ../src/;  make mode=opt all; cd ../tests')
   os.system('cd ../src/;  make mode=deb all; cd ../tests')
  
-  launcher = launch.pthLaunch()
+  launcher = launch.pthLaunch();
 
   # Filter tests <could be promoted into batch execute()/verify() methods
   args = launcher.args
   subset = []
   if args.test:
-    print(registeredTests)
+    #print(registeredTests)
     tnames = args.test.split(',')
     for name in tnames:
       for t in registeredTests:
@@ -51,8 +70,8 @@ def run_unittests_example1():
     if subset == []:
       raise RuntimeError('You requested to test a subset of registered tests, \n',
                          'but no registed test matched the name list provided')
-    else:
-      print(subset)
+    #else:
+    #  print(subset)
   else:
     subset = registeredTests
 
@@ -60,9 +79,11 @@ def run_unittests_example1():
   launcher.verifyTestSuite(subset)
 
 
+
+if __name__ == "__main__":
+  run_unittests_example1()
+
   # Clean current directory after executing the tests
   os.system('make clean')
 
 
-if __name__ == "__main__":
-  run_unittests_example1()
