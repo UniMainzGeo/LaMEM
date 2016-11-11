e649785809cb39542997d8aac1f39d3fac31921a
lapopov <popov@uni-mainz.de>
2016-11-11 11:18:11 +0100

    Enable setting command line options for preconditioning matrix. This is required to control SuperLU_DIST solver.

--- src/matrix.c
+++ src/matrix.c
@@ -81,6 +81,9 @@ PetscErrorCode MatAIJCreate(
 	ierr = MatMPIAIJSetPreallocation((*P), d_nz, d_nnz, o_nz, o_nnz); CHKERRQ(ierr);
 
 	// throw an error if preallocation fails
+
+	ierr = MatSetFromOptions((*P)); CHKERRQ(ierr);
+
 	ierr = MatSetOption((*P), MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_TRUE); CHKERRQ(ierr);
 	ierr = MatSetUp((*P)); CHKERRQ(ierr);
 
