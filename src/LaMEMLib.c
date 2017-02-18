321faaeefb098ab3bf1ad3224cd17342abe07639
lapopov <popov@uni-mainz.de>
2017-02-18 13:54:50 +0100

    *

--- src/LaMEMLib.c
+++ src/LaMEMLib.c
@@ -367,6 +367,52 @@ PetscErrorCode LaMEMLibSetLinks(LaMEMLib *lm)
 #define __FUNCT__ "LaMEMLibSaveOutput"
 PetscErrorCode LaMEMLibSaveOutput(LaMEMLib *lm, PetscInt dirInd)
 {
+/*
+	//==================
+	// Save data to disk
+	//==================
+
+
+
+-		if(!(JacResGetStep(&jr) % user.save_timesteps) || stop == PETSC_TRUE)
+
++		if(!(JacResGetStep(&jr) % user.save_timesteps) || stop == PETSC_TRUE || out_fail < 0)
+
+
+		if(!(JacResGetStep(&jr) % user.save_timesteps) || stop == PETSC_TRUE)
+		{
+			char *DirectoryName = NULL;
+
+			// redefine filename in case of inversion setup
+			if(IOparam != NULL)
+			{
+				asprintf(&DirectoryName, "Timestep_%1.6lld", (LLD)IOparam->mID);
+			}
+			else
+			{
+				// create directory (encode current time & step number)
+				asprintf(&DirectoryName, "Timestep_%1.6lld_%1.6e", (LLD)JacResGetStep(&jr), JacResGetTime(&jr));
+			}
+
+			ierr = LaMEMCreateOutputDirectory(DirectoryName); CHKERRQ(ierr);
+
+			// AVD phase output
+			ierr = PVAVDWriteTimeStep(&pvavd, DirectoryName, JacResGetTime(&jr), JacResGetStep(&jr)); CHKERRQ(ierr);
+
+			// grid ParaView output
+			ierr = PVOutWriteTimeStep(&pvout, &jr, DirectoryName, JacResGetTime(&jr), JacResGetStep(&jr)); CHKERRQ(ierr);
+
+			// free surface ParaView output
+			ierr = PVSurfWriteTimeStep(&pvsurf, DirectoryName, JacResGetTime(&jr), JacResGetStep(&jr)); CHKERRQ(ierr);
+
+			// marker ParaView output
+			ierr = PVMarkWriteTimeStep(&pvmark, DirectoryName, JacResGetTime(&jr), JacResGetStep(&jr)); CHKERRQ(ierr);
+
+			// clean up
+			if(DirectoryName) free(DirectoryName);
+		}
+
+*/
 
 	PetscFunctionReturn(0);
 }
@@ -381,7 +427,7 @@ PetscErrorCode LaMEMLibSolve(LaMEMLib *lm, void *param)
 	SNES     snes;   // PETSc nonlinear solver
 //	ObjFunct objf;   // objective function
 
-//	PetscBool      done;
+	PetscInt  done;
 
 	PetscErrorCode ierr;
 	PetscFunctionBegin;
@@ -390,321 +436,149 @@ PetscErrorCode LaMEMLibSolve(LaMEMLib *lm, void *param)
 //	ierr = ObjFunctCreate(&objf, IOparam, &surf); CHKERRQ(ierr);
 
 
-
 	// create Stokes preconditioner & matrix
-//	ierr = PMatCreate(&pm, &jr);    CHKERRQ(ierr);
+	ierr = PMatCreate(&pm, &lm->jr);    CHKERRQ(ierr);
 	ierr = PCStokesCreate(&pc, pm); CHKERRQ(ierr);
 
 	// create nonlinear solver
 	ierr = NLSolCreate(&nl, pc, &snes); CHKERRQ(ierr);
 
 
-	// set initial guess
-//	ierr = VecZeroEntries(jr->gsol); CHKERRQ(ierr);
-//	ierr = VecZeroEntries(jr->lT);   CHKERRQ(ierr);
-
-	// ...
-
-	ierr = PCStokesDestroy(pc);    CHKERRQ(ierr);
-	ierr = PMatDestroy    (pm);    CHKERRQ(ierr);
-	ierr = SNESDestroy    (&snes); CHKERRQ(ierr);
-	ierr = NLSolDestroy   (&nl);   CHKERRQ(ierr);
-//	ierr = ObjFunctDestroy(&objf);  CHKERRQ(ierr);
-
-	PetscFunctionReturn(0);
-}
-//---------------------------------------------------------------------------
-#undef __FUNCT__
-#define __FUNCT__ "LaMEMLibDryRun"
-PetscErrorCode LaMEMLibDryRun(LaMEMLib *lm)
-{
-
-//	PetscBool      done;
-
-	// set initial guess
-
-
-//	ierr = VecZeroEntries(jr->gsol); CHKERRQ(ierr);
-//	ierr = VecZeroEntries(jr->lT);   CHKERRQ(ierr);
-
-
-	PetscFunctionReturn(0);
-}
-
-
-
-
-
-
-//---------------------------------------------------------------------------
 /*
-{
-		// initialize variables
-		ierr = FDSTAGInitCode(&jr, &user, IOparam); CHKERRQ(ierr);
 
-		// check restart
-		ierr = BreakCheck(&user); CHKERRQ(ierr);
+	//===============
+	// TIME STEP LOOP
+	//===============
 
-		//======================
-		// SETUP DATA STRUCTURES
-		//======================
+//	PetscTime(&cputime_start_tstep);
+	// finish simulation if time_end was reached
+	if (jr.ts.istep >= jr.ts.nstep)
+	{
+	}
 
-		// initialize and setup scaling object, perform scaling
-		ierr = JacResInitScale(&jr, &user); CHKERRQ(ierr);
+// stop criterion must be evaluated here
+// time step increment - in the end of the step
 
-		// create staggered grid object
-		ierr = FDSTAGCreate(&fs, user.nnode_x, user.nnode_y, user.nnode_z); CHKERRQ(ierr);
+	do
+	{
+		PetscPrintf(PETSC_COMM_WORLD,"Time step %lld -------------------------------------------------------- \n", (LLD)JacResGetStep(&jr));
 
-		// generate coordinates of grid nodes/cells
-		ierr = FDSTAGGenCoord(&fs, &user); CHKERRQ(ierr);
+		//====================================
+		//	NONLINEAR THERMO-MECHANICAL SOLVER
+		//====================================
 
-		// save processor partitioning
-		if(user.SavePartitioning)
+		// initialize boundary constraint vectors
+		ierr = BCApply(&bc, jr.gsol); CHKERRQ(ierr);
+
+		// initialize temperature
+		ierr = JacResInitTemp(&jr); CHKERRQ(ierr);
+
+
+		if(user.SkipStokesSolver != PETSC_TRUE)
 		{
-			ierr = FDSTAGProcPartitioning(&fs, jr.scal.length); CHKERRQ(ierr);
+			PetscTime(&cputime_start_nonlinear);
 
-			// return immediately
-			ierr = FDSTAGDestroy(&fs); CHKERRQ(ierr);
+			PetscBool 			snes_convergence;
+			snes_convergence 	=	PETSC_FALSE;
 
-			PetscFunctionReturn(0);
+
+			// compute inverse elastic viscosities (dependent on dt)
+			ierr = JacResGetI2Gdt(&jr); CHKERRQ(ierr);
+
+			// solve nonlinear system with SNES
+			ierr = SNESSolve(snes, NULL, jr.gsol); CHKERRQ(ierr);
+
+			// print analyze convergence/divergence reason & iteration count
+			ierr = SNESPrintConvergedReason(snes, &snes_convergence); CHKERRQ(ierr);
+
+			if (!snes_convergence){
+
+				PetscPrintf(PETSC_COMM_WORLD, " **** Nonlinear solver failed to converge *** \n");
+
+			}
+
+			PetscTime(&cputime_end_nonlinear);
+
+			PetscPrintf(PETSC_COMM_WORLD, " Nonlinear solve took %g (sec)\n", cputime_end_nonlinear - cputime_start_nonlinear);
+		}
+		else
+		{
+			// just evaluate initial residual
+			ierr = FormResidual(snes, jr.gsol, jr.gres, &nl); CHKERRQ(ierr);
 		}
 
-		// print essential grid details
-		ierr = FDSTAGView(&fs); CHKERRQ(ierr);
-
-		// create boundary condition context
-		ierr = BCCreate(&bc, &fs, &jr.ts, &jr.scal); CHKERRQ(ierr);
-
-		// set boundary conditions parameters
-		ierr = BCSetParam(&bc, &user); CHKERRQ(ierr);
-
-		// set pushing block parameters
-		ierr = BCSetPush(&bc, &user); CHKERRQ(ierr);
-
-		// set parameters from PETSc options
-		ierr = BCReadFromOptions(&bc); CHKERRQ(ierr);
-
-		// overwrite grid info if restart and background strain-rates are applied - before marker init
-		// get rid of this!
-		if(user.restart == 1
-		&&(bc.ExxAct    == PETSC_TRUE
-		|| bc.EyyAct    == PETSC_TRUE))
+		// switch off initial guess flag
+		if(!JacResGetStep(&jr))
 		{
-			ierr = BreakReadGrid(&user, &fs); CHKERRQ(ierr);
+			jr.matLim.initGuess = 0;
 		}
 
-		// create Jacobian & residual evaluation context
-		ierr = JacResCreate(&jr, &fs, &bc); CHKERRQ(ierr);
+		// view nonlinear residual
+		ierr = JacResViewRes(&jr); CHKERRQ(ierr);
 
-		// create free surface grid
-		ierr = FreeSurfCreate(&surf, &jr, &user); CHKERRQ(ierr);
+		// select new time step
+		ierr = JacResGetCourantStep(&jr); CHKERRQ(ierr);
 
-		// initialize free surface from breakpoints if restart
-		if (user.restart == 1 && surf.UseFreeSurf == PETSC_TRUE) { ierr = BreakReadSurf(&fs, &surf); CHKERRQ(ierr); }
+		// prescribe velocity if rotation benchmark
+		if (user.msetup == ROTATION) {ierr = JacResSetVelRotation(&jr); CHKERRQ(ierr);}
 
-		ierr = BCSetupBoundVel(&bc, surf.InitLevel); CHKERRQ(ierr);
+		//==========================================
+		// MARKER & FREE SURFACE ADVECTION + EROSION
+		//==========================================
 
-		// create advection context
-		ierr = ADVCreate(&actx, &fs, &jr); CHKERRQ(ierr);
+		// advect free surface
+		ierr = FreeSurfAdvect(&surf); CHKERRQ(ierr);
 
-		// initialize markers
-		ierr = ADVMarkInit(&actx, &user); CHKERRQ(ierr);
+		// advect markers
+		ierr = ADVAdvect(&actx); CHKERRQ(ierr);
 
-		// initialize influx markers
-		ierr = ADVMarkInitInfluxBC(&actx, &user); CHKERRQ(ierr);
+		// apply background strain-rate "DWINDLAR" BC (Bob Shaw "Ship of Strangers")
+		ierr = BCStretchGrid(&bc); CHKERRQ(ierr);
 
-		// change marker phase when crossing free surface
-		ierr = ADVMarkCrossFreeSurf(&actx, &surf, 0.05); CHKERRQ(ierr);
+		// exchange markers between the processors (after mesh advection)
+		ierr = ADVExchange(&actx); CHKERRQ(ierr);
 
-		// set air phase to properly treat marker advection & temperature diffusion
-		if(surf.UseFreeSurf == PETSC_TRUE && jr.actTemp)
-		{
-			actx.AirPhase = surf.AirPhase;
-			jr.AirPhase   = surf.AirPhase;
-			actx.Ttop     = bc.Ttop;
-		}
+		// apply erosion to the free surface
+		ierr = FreeSurfAppErosion(&surf); CHKERRQ(ierr);
 
-		// check thermal material parameters
-		ierr = JacResCheckTempParam(&jr); CHKERRQ(ierr);
+		// apply sedimentation to the free surface
+		ierr = FreeSurfAppSedimentation(&surf); CHKERRQ(ierr);
+
+		// remap markers onto (stretched) grid
+		ierr = ADVRemap(&actx, &surf); CHKERRQ(ierr);
 
 		// update phase ratios taking into account actual free surface position
 		ierr = FreeSurfGetAirPhaseRatio(&surf); CHKERRQ(ierr);
 
-		// create Stokes preconditioner & matrix
-		ierr = PMatCreate(&pm, &jr);    CHKERRQ(ierr);
-		ierr = PCStokesCreate(&pc, pm); CHKERRQ(ierr);
+		// advect pushing block
+		ierr = BCAdvectPush(&bc); CHKERRQ(ierr);
 
-		// create nonlinear solver
-		ierr = NLSolCreate(&nl, pc, &snes); CHKERRQ(ierr);
+		// compute gravity misfits
+//		ierr = CalculateMisfitValues(&user, C, itime, LaMEM_OutputParameters); CHKERRQ(ierr);
 
-		// create output object for all requested output variables
-		ierr = PVOutCreate(&pvout, &jr, user.OutputFile); CHKERRQ(ierr);
+		// ACHTUNG !!!
+		PetscBool          flg;
+		KSP                ksp;
+		KSPConvergedReason reason;
+		PetscBool          stop = PETSC_FALSE;
 
-		// create output object for the free surface
-		ierr = PVSurfCreate(&pvsurf, &surf, user.OutputFile); CHKERRQ(ierr);
+		ierr = PetscOptionsHasName(NULL, NULL, "-stop_linsol_fail", &flg); CHKERRQ(ierr);
 
-		// create output object for the markers - for debugging
-		ierr = PVMarkCreate(&pvmark, &actx, user.OutputFile); CHKERRQ(ierr);
-
-		// AVD output driver
-		ierr = PVAVDCreate(&pvavd, &actx, user.OutputFile); CHKERRQ(ierr);
-
-		// create objective function object
-		ierr = ObjFunctCreate(&objf, IOparam, &surf); CHKERRQ(ierr);
-
-		// read breakpoint files if restart was requested and if is possible
-		if (user.restart==1) { ierr = BreakRead(&user, &actx, &pvout, &pvsurf, &pvmark, &pvavd, &nl.jtype); CHKERRQ(ierr); }
-
-		// finish simulation if time_end was reached
-		if (jr.ts.istep >= jr.ts.nstep)
+		if(flg == PETSC_TRUE)
 		{
-			// cleanup
-			ierr = FDSTAGDestroy(&fs);     CHKERRQ(ierr);
-			ierr = FreeSurfDestroy(&surf); CHKERRQ(ierr);
-			ierr = BCDestroy(&bc);         CHKERRQ(ierr);
-			ierr = JacResDestroy(&jr);     CHKERRQ(ierr);
-			ierr = ADVDestroy(&actx);      CHKERRQ(ierr);
-			ierr = PCStokesDestroy(pc);    CHKERRQ(ierr);
-			ierr = PMatDestroy(pm);        CHKERRQ(ierr);
-			ierr = SNESDestroy(&snes);     CHKERRQ(ierr);
-			ierr = NLSolDestroy(&nl);      CHKERRQ(ierr);
-			ierr = PVOutDestroy(&pvout);   CHKERRQ(ierr);
-			ierr = PVSurfDestroy(&pvsurf); CHKERRQ(ierr);
-			ierr = PVMarkDestroy(&pvmark); CHKERRQ(ierr);
-			ierr = PVAVDDestroy(&pvavd);   CHKERRQ(ierr);
-			ierr = ObjFunctDestroy(&objf); CHKERRQ(ierr);
+			ierr = SNESGetKSP(snes, &ksp); CHKERRQ(ierr);
 
-			PetscFunctionReturn(0);
+			ierr = KSPGetConvergedReason(ksp, &reason);
+
+			if(reason == KSP_DIVERGED_ITS)
+			{
+				stop = PETSC_TRUE;
+			}
 		}
 
-		PetscPrintf(PETSC_COMM_WORLD," \n");
-
-		//===============
-		// TIME STEP LOOP
-		//===============
-
-	//	PetscTime(&cputime_start_tstep);
-
-		do
-		{
-			PetscPrintf(PETSC_COMM_WORLD,"Time step %lld -------------------------------------------------------- \n", (LLD)JacResGetStep(&jr));
-
-			//====================================
-			//	NONLINEAR THERMO-MECHANICAL SOLVER
-			//====================================
-
-			// initialize boundary constraint vectors
-			ierr = BCApply(&bc, jr.gsol); CHKERRQ(ierr);
-
-			// initialize temperature
-			ierr = JacResInitTemp(&jr); CHKERRQ(ierr);
-
-
-			if(user.SkipStokesSolver != PETSC_TRUE)
-			{
-				PetscTime(&cputime_start_nonlinear);
-
-				PetscBool 			snes_convergence;
-				snes_convergence 	=	PETSC_FALSE;
-
-
-				// compute inverse elastic viscosities (dependent on dt)
-				ierr = JacResGetI2Gdt(&jr); CHKERRQ(ierr);
-
-				// solve nonlinear system with SNES
-				ierr = SNESSolve(snes, NULL, jr.gsol); CHKERRQ(ierr);
-
-				// print analyze convergence/divergence reason & iteration count
-				ierr = SNESPrintConvergedReason(snes, &snes_convergence); CHKERRQ(ierr);
-
-				if (!snes_convergence){
-
-					PetscPrintf(PETSC_COMM_WORLD, " **** Nonlinear solver failed to converge *** \n");
-
-				}
-
-				PetscTime(&cputime_end_nonlinear);
-
-				PetscPrintf(PETSC_COMM_WORLD, " Nonlinear solve took %g (sec)\n", cputime_end_nonlinear - cputime_start_nonlinear);
-			}
-			else
-			{
-				// just evaluate initial residual
-				ierr = FormResidual(snes, jr.gsol, jr.gres, &nl); CHKERRQ(ierr);
-			}
-
-			// switch off initial guess flag
-			if(!JacResGetStep(&jr))
-			{
-				jr.matLim.initGuess = 0;
-			}
-
-			// view nonlinear residual
-			ierr = JacResViewRes(&jr); CHKERRQ(ierr);
-
-			// select new time step
-			ierr = JacResGetCourantStep(&jr); CHKERRQ(ierr);
-
-			// prescribe velocity if rotation benchmark
-			if (user.msetup == ROTATION) {ierr = JacResSetVelRotation(&jr); CHKERRQ(ierr);}
-
-			//==========================================
-			// MARKER & FREE SURFACE ADVECTION + EROSION
-			//==========================================
-
-			// advect free surface
-			ierr = FreeSurfAdvect(&surf); CHKERRQ(ierr);
-
-			// advect markers
-			ierr = ADVAdvect(&actx); CHKERRQ(ierr);
-
-			// apply background strain-rate "DWINDLAR" BC (Bob Shaw "Ship of Strangers")
-			ierr = BCStretchGrid(&bc); CHKERRQ(ierr);
-
-			// exchange markers between the processors (after mesh advection)
-			ierr = ADVExchange(&actx); CHKERRQ(ierr);
-
-			// apply erosion to the free surface
-			ierr = FreeSurfAppErosion(&surf); CHKERRQ(ierr);
-
-			// apply sedimentation to the free surface
-			ierr = FreeSurfAppSedimentation(&surf); CHKERRQ(ierr);
-
-			// remap markers onto (stretched) grid
-			ierr = ADVRemap(&actx, &surf); CHKERRQ(ierr);
-
-			// update phase ratios taking into account actual free surface position
-			ierr = FreeSurfGetAirPhaseRatio(&surf); CHKERRQ(ierr);
-
-			// advect pushing block
-			ierr = BCAdvectPush(&bc); CHKERRQ(ierr);
-
-			// compute gravity misfits
-	//		ierr = CalculateMisfitValues(&user, C, itime, LaMEM_OutputParameters); CHKERRQ(ierr);
-
-			// ACHTUNG !!!
-			PetscBool          flg;
-			KSP                ksp;
-			KSPConvergedReason reason;
-			PetscBool          stop = PETSC_FALSE;
-
-			ierr = PetscOptionsHasName(NULL, NULL, "-stop_linsol_fail", &flg); CHKERRQ(ierr);
-
-			if(flg == PETSC_TRUE)
-			{
-				ierr = SNESGetKSP(snes, &ksp); CHKERRQ(ierr);
-
-				ierr = KSPGetConvergedReason(ksp, &reason);
-
-				if(reason == KSP_DIVERGED_ITS)
-				{
-					stop = PETSC_TRUE;
-				}
-			}
-
-			//==================
-			// Save data to disk
-			//==================
+		//==================
+		// Save data to disk
+		//==================
 +		// ACHTUNG !!!
 
 +		SNESConvergedReason snes_reason;
@@ -719,77 +593,51 @@ PetscErrorCode LaMEMLibDryRun(LaMEMLib *lm)
 
 +
 
- 		//==================
-
- 		// Save data to disk
-
- 		//==================
-
-
-
--		if(!(JacResGetStep(&jr) % user.save_timesteps) || stop == PETSC_TRUE)
-
-+		if(!(JacResGetStep(&jr) % user.save_timesteps) || stop == PETSC_TRUE || out_fail < 0)
-
-
-			if(!(JacResGetStep(&jr) % user.save_timesteps) || stop == PETSC_TRUE)
-			{
-				char *DirectoryName = NULL;
-
-				// redefine filename in case of inversion setup
-				if(IOparam != NULL)
-				{
-					asprintf(&DirectoryName, "Timestep_%1.6lld", (LLD)IOparam->mID);
-				}
-				else
-				{
-					// create directory (encode current time & step number)
-					asprintf(&DirectoryName, "Timestep_%1.6lld_%1.6e", (LLD)JacResGetStep(&jr), JacResGetTime(&jr));
-				}
-
-				ierr = LaMEMCreateOutputDirectory(DirectoryName); CHKERRQ(ierr);
-
-				// AVD phase output
-				ierr = PVAVDWriteTimeStep(&pvavd, DirectoryName, JacResGetTime(&jr), JacResGetStep(&jr)); CHKERRQ(ierr);
-
-				// grid ParaView output
-				ierr = PVOutWriteTimeStep(&pvout, &jr, DirectoryName, JacResGetTime(&jr), JacResGetStep(&jr)); CHKERRQ(ierr);
-
-				// free surface ParaView output
-				ierr = PVSurfWriteTimeStep(&pvsurf, DirectoryName, JacResGetTime(&jr), JacResGetStep(&jr)); CHKERRQ(ierr);
-
-				// marker ParaView output
-				ierr = PVMarkWriteTimeStep(&pvmark, DirectoryName, JacResGetTime(&jr), JacResGetStep(&jr)); CHKERRQ(ierr);
-
-				// clean up
-				if(DirectoryName) free(DirectoryName);
-			}
-
 			if(stop == PETSC_TRUE)
-			{
-				SETERRQ(PETSC_COMM_SELF, PETSC_ERR_USER, "Linear solver failed. Terminating simulation.\n");
-			}
+		{
+			SETERRQ(PETSC_COMM_SELF, PETSC_ERR_USER, "Linear solver failed. Terminating simulation.\n");
+		}
 
-			// store markers to disk
-			ierr = ADVMarkSave(&actx, &user);  CHKERRQ(ierr);
+		// store markers to disk
+		ierr = ADVMarkSave(&actx, &user);  CHKERRQ(ierr);
 
-			// update time state
-			ierr = TSSolUpdate(&jr.ts, &jr.scal, &done); CHKERRQ(ierr);
+		// update time state
+		ierr = TSSolUpdate(&jr.ts, &jr.scal, &done); CHKERRQ(ierr);
 
-			// create BREAKPOINT files, for restarting the code
-			if(user.save_breakpoints>0 && !((JacResGetStep(&jr)-1) % user.save_breakpoints))
-			{
-				ierr = BreakWrite(&user, &actx, &surf, &pvout, &pvsurf, &pvmark, &pvavd, nl.jtype); CHKERRQ(ierr);
-			}
+		// create BREAKPOINT files, for restarting the code
+		if(user.save_breakpoints>0 && !((JacResGetStep(&jr)-1) % user.save_breakpoints))
+		{
+			ierr = BreakWrite(&user, &actx, &surf, &pvout, &pvsurf, &pvmark, &pvavd, nl.jtype); CHKERRQ(ierr);
+		}
 
-			// check marker phases
-			ierr = ADVCheckMarkPhases(&actx, jr.numPhases); CHKERRQ(ierr);
+		// check marker phases
+		ierr = ADVCheckMarkPhases(&actx, jr.numPhases); CHKERRQ(ierr);
 
-		} while(done != PETSC_TRUE);
+	} while(done != PETSC_TRUE);
 
-		//======================
-		// END OF TIME STEP LOOP
-		//======================
-}
+	//======================
+	// END OF TIME STEP LOOP
+	//======================
+
 
 */
+
+	ierr = PCStokesDestroy(pc);    CHKERRQ(ierr);
+	ierr = PMatDestroy    (pm);    CHKERRQ(ierr);
+	ierr = SNESDestroy    (&snes); CHKERRQ(ierr);
+	ierr = NLSolDestroy   (&nl);   CHKERRQ(ierr);
+//	ierr = ObjFunctDestroy(&objf); CHKERRQ(ierr);
+
+	PetscFunctionReturn(0);
+}
+//---------------------------------------------------------------------------
+#undef __FUNCT__
+#define __FUNCT__ "LaMEMLibDryRun"
+PetscErrorCode LaMEMLibDryRun(LaMEMLib *lm)
+{
+
+
+
+	PetscFunctionReturn(0);
+}
+//---------------------------------------------------------------------------
