76c18d02c543224ae7ef1440c986bda79af9632e
Boris Kaus <kaus@uni-mainz.de>
2018-01-26 00:03:03 +0100

    starting a cleanup of the input files in /input_models & adding a testing framework (to make it easier to test whether the input files still work)

---
+++ input_models/BuildInSetups/Tests/test_BuildInSetup.py
@@ -0,0 +1,162 @@
+
+import os
+import pyTestHarness.unittest as pth
+import pyTestHarness.launcher as launch
+import re
+
+
+def makeLocalPathAbsolute(localRelPath) :
+  thisDir = os.path.split(os.path.abspath(__file__))[0]
+  return(os.path.join(thisDir,localRelPath))
+
+#------------------------------------------------------------------------------------------------
+def test_1():
+
+  # Test a falling block case with build-in direct solver on 1 core, using optimized LaMEM
+  ranks  = 1
+  launch =  makeLocalPathAbsolute('../../../bin/opt/LaMEM -ParamFile ./BuildInSetups/FallingBlock_DirectSolver.dat -nstep_max 3 -dt_out 0 -nstep_ini 0') 
+  expected_file = makeLocalPathAbsolute('FallingBlock_DirectSolver-p1.expected')
+
+  def comparefunc(unittest):
+
+    key = re.escape("|Div|_inf")
+    unittest.compareFloatingPoint(key,1e-7)
+
+    key = re.escape("|Div|_2")
+    unittest.compareFloatingPoint(key,1e-6)
+
+    key = re.escape("|mRes|_2")
+    unittest.compareFloatingPoint(key,1e-6)
+
+    key = 'CONVERGED_RTOL iterations'
+    unittest.compareInteger(key,0)
+
+  # Create unit test object
+  test = pth.pthUnitTest('FallingBlock_Direct_MUMPS_serial',ranks,launch,expected_file)
+  test.setVerifyMethod(comparefunc)
+  test.appendKeywords('@')
+
+  return(test)
+
+#------------------------------------------------------------------------------------------------
+def test_2():
+
+  # Test a falling block case with SUPERLU_DIST direct solver
+  ranks = 2
+  launch =  makeLocalPathAbsolute('../../../bin/opt/LaMEM -ParamFile ./BuildInSetups/FallingBlock_DirectSolver.dat -nstep_max 3 -dt_out 0 -nstep_ini 0 -jp_pc_factor_mat_solver_package superlu_dist') 
+  expected_file = makeLocalPathAbsolute('FallingBlock_DirectSolver-p2.expected')
+
+  def comparefunc(unittest):
+
+    key = re.escape("|Div|_inf")
+    unittest.compareFloatingPoint(key,1e-7)
+
+    key = re.escape("|Div|_2")
+    unittest.compareFloatingPoint(key,1e-6)
+
+    key = re.escape("|mRes|_2")
+    unittest.compareFloatingPoint(key,1e-6)
+
+    key = 'CONVERGED_RTOL iterations'
+    unittest.compareInteger(key,0)
+
+  # Create unit test object
+  test = pth.pthUnitTest('FallingBlock_Direct_SUPERLU_DIST',ranks,launch,expected_file)
+  test.setVerifyMethod(comparefunc)
+  test.appendKeywords('@')
+
+  return(test)
+
+#------------------------------------------------------------------------------------------------
+def test_3():
+  # Falling block setup with multigrid solver
+  ranks = 2
+  launch =  makeLocalPathAbsolute('../../../bin/opt/LaMEM -ParamFile ./BuildInSetups/FallingBlock_Multigrid.dat -nstep_max 3 -dt_out 0 -nstep_ini 0') 
+  expected_file = makeLocalPathAbsolute('FallingBlock_Multigrid-p2.expected')
+
+  def comparefunc(unittest):
+
+    key = re.escape("|Div|_inf")
+    unittest.compareFloatingPoint(key,1e-7)
+
+    key = re.escape("|Div|_2")
+    unittest.compareFloatingPoint(key,1e-6)
+
+    key = re.escape("|mRes|_2")
+    unittest.compareFloatingPoint(key,1e-6)
+
+    key = 'CONVERGED_RTOL iterations'
+    unittest.compareInteger(key,0)
+
+    key = 'KSP Residual norm'
+    unittest.compareFloatingPoint(key,1e-6)
+    
+  # Create unit test object
+  test = pth.pthUnitTest('FallingBlock_Multigrid',ranks,launch,expected_file)
+  test.setVerifyMethod(comparefunc)
+  test.appendKeywords('@')
+
+  return(test)
+
+
+#------------------------------------------------------------------------------------------------
+def test_4():
+  # Falling spheres setup with multigrid solver
+  ranks = 2
+  launch =  makeLocalPathAbsolute('../../../bin/opt/LaMEM -ParamFile ./BuildInSetups/FallingSpheres_Multigrid.dat -dt_out 0 -nstep_ini 0') 
+  expected_file = makeLocalPathAbsolute('FallingSpheres_Multigrid-p2.expected')
+
+  def comparefunc(unittest):
+
+    key = re.escape("|Div|_inf")
+    unittest.compareFloatingPoint(key,1e-7)
+
+    key = re.escape("|Div|_2")
+    unittest.compareFloatingPoint(key,1e-6)
+
+    key = re.escape("|mRes|_2")
+    unittest.compareFloatingPoint(key,1e-6)
+
+    key = 'CONVERGED_RTOL iterations'
+    unittest.compareInteger(key,0)
+
+    key = 'KSP Residual norm'
+    unittest.compareFloatingPoint(key,1e-6)
+    
+  # Create unit test object
+  test = pth.pthUnitTest('FallingSpheres_Multigrid',ranks,launch,expected_file)
+  test.setVerifyMethod(comparefunc)
+  test.appendKeywords('@')
+
+  return(test)
+
+#------------------------------------------------------------------------------------------------
+def test_5():
+  # Falling spheres setup with multigrid solver but jacobi smoother
+  ranks = 2
+  launch =  makeLocalPathAbsolute('../../../bin/opt/LaMEM -ParamFile ./BuildInSetups/FallingSpheres_Multigrid.dat -gmg_mg_levels_ksp_type richardson -gmg_mg_levels_pc_type jacobi -gmg_mg_levels_ksp_richardson_scale 0.5') 
+  expected_file = makeLocalPathAbsolute('FallingSpheres_Multigrid-Jacobi-p2.output')
+
+  def comparefunc(unittest):
+
+    key = re.escape("|Div|_inf")
+    unittest.compareFloatingPoint(key,1e-7)
+
+    key = re.escape("|Div|_2")
+    unittest.compareFloatingPoint(key,1e-6)
+
+    key = re.escape("|mRes|_2")
+    unittest.compareFloatingPoint(key,1e-6)
+
+    key = 'CONVERGED_RTOL iterations'
+    unittest.compareInteger(key,0)
+
+    key = 'KSP Residual norm'
+    unittest.compareFloatingPoint(key,1e-6)
+    
+  # Create unit test object
+  test = pth.pthUnitTest('FallingSpheres_Multigrid_Jacobi',ranks,launch,expected_file)
+  test.setVerifyMethod(comparefunc)
+  test.appendKeywords('@')
+
+  return(test)
---
+++ input_models/verify_InputModels.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import os,sys
+import subprocess
+sys.path.insert(0, "../tests/pythontestharness/lib")
+
+# This is a testing framework to ensure that the files in the LaMEM repository
+# in /input_models remain giving consistent results
+
+import argparse
+
+#import pyTestHarness.unittest as pth
+import pyTestHarness.harness as pthharness
+import pyTestHarness.launcher as launch
+
+# Build optimized and debug versions of LaMEM
+os.system('cd ../src/;  make mode=opt all; cd ../input_models')
+os.system('cd ../src/;  make mode=deb all; cd ../input_models')
+
+# Import the tests that are in each of the directories in /input_models
+sys.path.append(os.path.join(os.environ['PWD'], 'BuildInSetups/Tests'))
+
+# add matlab-tests if matlab is available as ENVIRONMENTAL variable MATLAB
+#if os.environ.get('MATLAB') != None:
+#    sys.path.append(os.path.join(os.environ['PWD'], 't3_SubductionMATLABinput'))
+#else:
+#   print('MATLAB tests cannot be executed, as the environmental variable $MATLAB is not set')
+
+import test_BuildInSetup as BuildIn
+
+#if os.environ.get('MATLAB') != None:
+#  import test_3_Subduction1               as Sub1 # import test that requires MATLAB
+ 
+def makeLocalPathAbsolute(localRelPath) :
+  thisDir = os.path.split(os.path.abspath(__file__))[0]
+  return(os.path.join(thisDir,localRelPath))
+
+def run_tests():
+  os.environ['PYTHONUNBUFFERED'] = str('1')
+
+  registeredTests = [ BuildIn.test_1(), BuildIn.test_2(), BuildIn.test_3(), BuildIn.test_4(),
+                      BuildIn.test_5()];
+ 
+  if os.path.isdir('TestOutput') == False:
+    os.mkdir('TestOutput')
+
+  for test in registeredTests:
+    test.setOutputPath(makeLocalPathAbsolute('TestOutput'))
+
+# Add matlab tests (There should be a better way to do this for a range of files at the same time)
+  #if os.environ.get('MATLAB') != None:
+  #  registeredTests.append(Sub1.test_a());
+  
+
+  # Run the tests:
+  h = pthharness.Harness(registeredTests)
+  h.execute()
+  h.verify()
+
+
+if __name__ == "__main__":
+  run_tests()
+
+  # Cleanup
+  os.system('rm -rf Timestep*')
+  os.system('rm -rf *.pvd')
+
