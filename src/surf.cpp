f1bb2d8741156615d560df08034d2fa5fe679324
Boris Kaus <kaus@uni-mainz.de>
2018-02-22 17:31:17 +0100

    typo; sedimentation rates are in cm/yr if we specify GEO units (and m/s in case of SI units)

--- src/surf.cpp
+++ src/surf.cpp
@@ -97,7 +97,7 @@ PetscErrorCode FreeSurfCreate(FreeSurf *surf, FB *fb)
 		// sedimentation model parameters
 		ierr = getIntParam   (fb, _REQUIRED_, "sed_num_layers",  &surf->numLayers,  1,                 _max_layers_);      CHKERRQ(ierr);
 		ierr = getScalarParam(fb, _REQUIRED_, "sed_time_delims",  surf->timeDelims, surf->numLayers-1, scal->time);        CHKERRQ(ierr);
-		ierr = getScalarParam(fb, _REQUIRED_, "sed_rates",        surf->sedRates,   surf->numLayers,   scal->strain_rate); CHKERRQ(ierr);
+		ierr = getScalarParam(fb, _REQUIRED_, "sed_rates",        surf->sedRates,   surf->numLayers,   scal->velocity);    CHKERRQ(ierr);
 		ierr = getIntParam   (fb, _REQUIRED_, "sed_phases",       surf->sedPhases,  surf->numLayers,   maxPhaseID);        CHKERRQ(ierr);
 	}
 
