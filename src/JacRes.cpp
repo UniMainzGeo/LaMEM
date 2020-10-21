93834297ab6935fb7901f430ca8632ee0dffe0f0
APiccolo89 <piccolo@uni-mainz.de>
2020-10-21 17:25:41 +0200

    I commit the first stage of the work. Still need to test it, but at least it compiles nicely. I perform maketest and there are three test that fails: [0]PETSC ERROR: No such diffusion creep profile: Plagioclase_RybackiDresen_2000! Now I check the reason why

--- src/JacRes.cpp
+++ src/JacRes.cpp
@@ -132,6 +132,8 @@ PetscErrorCode JacResCreate(JacRes *jr, FB *fb)
 	ierr = getIntParam   (fb, _OPTIONAL_, "lmaxit",          &ctrl->lmaxit,         1, 1000);       CHKERRQ(ierr);
 	ierr = getScalarParam(fb, _OPTIONAL_, "lrtol",           &ctrl->lrtol,          1, 1.0);        CHKERRQ(ierr);
     ierr = getIntParam   (fb, _OPTIONAL_, "Phasetrans",      &ctrl->Phasetrans,     1, 1);          CHKERRQ(ierr);
+    ierr = getIntParam   (fb, _OPTIONAL_, "Passive_Tracers", &ctrl->Passive_Tracer, 1, 1);          CHKERRQ(ierr);
+
 
 	if     (!strcmp(gwtype, "none"))  ctrl->gwType = _GW_NONE_;
 	else if(!strcmp(gwtype, "top"))   ctrl->gwType = _GW_TOP_;
@@ -288,7 +290,8 @@ PetscErrorCode JacResCreate(JacRes *jr, FB *fb)
 	if(ctrl->mfmax)          PetscPrintf(PETSC_COMM_WORLD, "   Max. melt fraction (viscosity, density) : %g    \n", ctrl->mfmax);
 	if(ctrl->lmaxit)         PetscPrintf(PETSC_COMM_WORLD, "   Rheology iteration number               : %lld  \n", ctrl->lmaxit);
 	if(ctrl->lrtol)          PetscPrintf(PETSC_COMM_WORLD, "   Rheology iteration tolerance            : %g    \n", ctrl->lrtol);
-	if(ctrl->Phasetrans)          PetscPrintf(PETSC_COMM_WORLD, "   Phase transition is active           : %g    \n", ctrl->Phasetrans);
+	if(ctrl->Phasetrans)     PetscPrintf(PETSC_COMM_WORLD, "   Phase transition is active           : %g    \n", ctrl->Phasetrans);
+	if(ctrl->Passive_Tracer) PetscPrintf(PETSC_COMM_WORLD, "   Passive Tracers are active           : %g    \n", ctrl->Passive_Tracer);
 
 	PetscPrintf(PETSC_COMM_WORLD, "   Ground water level type                 : ");
 	if     (ctrl->gwType == _GW_NONE_)  PetscPrintf(PETSC_COMM_WORLD, "none \n");
