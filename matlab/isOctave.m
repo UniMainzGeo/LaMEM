c55e2566eab393781ffd8f1687ba8bcd3dd40c94
Boris Kaus <kaus@uni-mainz.de>
2020-11-05 15:25:55 +0100

    important for older matlab versions

--- matlab/isOctave.m
+++ matlab/isOctave.m
@@ -5,7 +5,7 @@ function retval = isOctave
   persistent cacheval;  % speeds up repeated calls
 
   if isempty (cacheval)
-    cacheval = (exist ("OCTAVE_VERSION", "builtin") > 0);
+    cacheval = (exist ('OCTAVE_VERSION', 'builtin') > 0);
   end
 
   retval = cacheval;
