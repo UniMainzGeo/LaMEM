a39cf9908319c98dcfedca4e8c76d07e9778c07e
Boris Kaus <kaus@uni-mainz.de>
2020-11-08 20:01:58 +0100

    added a FSSA benchmark as well as a Julia plotting script to compare it with the original solution

--- tests/runLaMEM_Tests.py
+++ tests/runLaMEM_Tests.py
@@ -30,7 +30,7 @@ sys.path.append(os.path.join(os.environ['PWD'], 't15_RTI'))
 sys.path.append(os.path.join(os.environ['PWD'], 't16_PhaseTransitions'))
 sys.path.append(os.path.join(os.environ['PWD'], 't17_InflowOutflow'))
 sys.path.append(os.path.join(os.environ['PWD'], 't18_SimpleShear'))
-
+sys.path.append(os.path.join(os.environ['PWD'], 't20_FSSA'))
 
 # add matlab-tests if matlab is available as ENVIRONMENTAL variable MATLAB
 if os.environ.get('MATLAB') != None:
@@ -55,6 +55,8 @@ import test_15_RTI as RTI
 import test_16_PhaseTransitions as PT
 import test_17_InflowOutflow as InOut
 import test_18_SimpleShear as SS
+import test_20_FSSA as FSSA
+
 
 
 if os.environ.get('MATLAB') != None:
@@ -83,7 +85,8 @@ def run_tests():
                       Rheology0D.LinearViscous(),  Rheology0D.DislocationCreeplaw(), Rheology0D.ViscoElastic_DislocationCreep(),
                       StrEnv.test_a(), StrEnv.test_b(), StrEnv.test_c(), StrEnv.test_d(),
                       RTI.RTI_isovisous_NoSlip(), PT.test_a(),
-                      InOut.test_2D(), InOut.test_3D(), SS.test_xz(), SS.test_yz(), SS.test_xy(), SS.test_xz_yz()];
+                      InOut.test_2D(), InOut.test_3D(), SS.test_xz(), SS.test_yz(), SS.test_xy(), SS.test_xz_yz(),
+                      FSSA.test_1()];
  
 
 # Add matlab tests (There should be a better way to do this for a range of files at the same time)
