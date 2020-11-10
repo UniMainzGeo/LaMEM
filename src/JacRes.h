0fcc86efdfd2abe578dca6d03bc30d7ca50bcb97
Boris Kaus <kaus@uni-mainz.de>
2020-11-10 18:02:45 +0100

    bugfixes to make it compile. note that adv_C does not seem to be initialized

--- src/JacRes.h
+++ src/JacRes.h
@@ -183,6 +183,7 @@ struct Controls
 	PetscScalar lrtol;          // local rheology iterations relative tolerance
 	PetscInt    Phasetrans; // Flag to activate phase transition routines
 	PetscInt    Passive_Tracer; // Flag to activate passive tracer routine
+	PetscInt 	adv_C;			//??
 
 };
 
