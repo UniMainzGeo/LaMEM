570950e4ab2b88a26911482d404839121de51f90
Boris Kaus <kaus@uni-mainz.de>
2020-11-17 17:22:39 +0100

    added the possibility to create a phase transition based on the melt fraction within a control volume. This also gives a code example on how one can incorporate phase transitions that are based on an arbitrary field that is present within svBulk (the grid), and not just based on parameters that exist within the marker structure.

--- tests/t16_PhaseTransitions/test_16_PhaseTransitions.py
+++ tests/t16_PhaseTransitions/test_16_PhaseTransitions.py
@@ -56,3 +56,32 @@ def test_b():
   ex1.appendKeywords('@')
 
   return(ex1)
+
+
+def test_c():
+
+  # Case with partial melting and a phase transition that depends on the amoujnt of partial melting 
+  ranks = 1
+  launch = '../bin/opt/LaMEM -ParamFile ./t16_PhaseTransitions/Plume_PhaseTransitions_Melting.dat' # This must be a relative path with respect to runLaMEM_Tests.py
+  expected_file = 't16_PhaseTransitions/PhaseTransitions-Melting_p1.expected'
+
+  def comparefunc(unittest):
+
+    key = re.escape("|Div|_inf")
+    unittest.compareFloatingPoint(key,1e-7)
+
+    key = re.escape("|Div|_2")
+    unittest.compareFloatingPoint(key,1e-5)
+
+    key = re.escape("|mRes|_2")
+    unittest.compareFloatingPoint(key,1e-4)
+
+    key = re.escape("|eRes|_2")
+    unittest.compareFloatingPoint(key,1e-6)
+
+  # Create unit test object
+  ex1 = pth.pthUnitTest('t16_PhaseTransitions_Melting_opt',ranks,launch,expected_file)
+  ex1.setVerifyMethod(comparefunc)
+  ex1.appendKeywords('@')
+
+  return(ex1)
