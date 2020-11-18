310c55c1536461a4a7c269aaa7f36afb7fe05777
Boris Kaus <kaus@uni-mainz.de>
2020-11-18 14:49:10 +0100

    changed the script to make it clearer what should NOT be passed to LaMEM

--- tests/t9_PhaseDiagrams/BenchmarkPhaseDiagrams.m
+++ tests/t9_PhaseDiagrams/BenchmarkPhaseDiagrams.m
@@ -16,8 +16,8 @@ M = dlmread(PD_name, ' ', 55, 0);
 rho         =   reshape(M(:,3),np,nt);
 rho_fluid   =   reshape(M(:,1),np,nt);
 melt        =   reshape(M(:,2),np,nt);
-T           =   reshape(M(:,4),np,nt);
-P           =   reshape(M(:,5),np,nt)./1e3;
+T           =   reshape(M(:,4),np,nt);       % degree C
+P           =   reshape(M(:,5),np,nt)./1e3;  % kbar  - LaMEM input must be bar!
 
 % Clear strange values in the PD and give warnings
 ind = find(rho == 0);
@@ -43,87 +43,46 @@ end
 
 if sym == 1
     % symmetrize
-    rho = rho(1:end-1,1:end-1);
-    rho_fluid = rho_fluid(1:end-1,1:end-1);
-    melt = melt(1:end-1,1:end-1);
-    T = T(1:end-1,1:end-1);
-    P = P(1:end-1,1:end-1);
+    rho         = rho(1:end-1,1:end-1);
+    rho_fluid   = rho_fluid(1:end-1,1:end-1);
+    melt        = melt(1:end-1,1:end-1);
+    T           = T(1:end-1,1:end-1);
+    P           = P(1:end-1,1:end-1);
 end
 
 figure(1),clf
+subplot(221)
 pcolor(T,P,rho)
 shading interp
 colorbar
-ylabel('P')
+ylabel('P [kbar]')
 xlabel('T [Celcius]')
-title('density rock w/out melt')
+title('density rock w/out melt!')
 
-figure(2),clf
+subplot(222)
 pcolor(T,P,melt)
 shading interp
 colorbar
-ylabel('P')
+ylabel('P [kbar]')
 xlabel('T [Celcius]')
 title('Melt content')
 
-figure(3),clf
+subplot(223)
 pcolor(T,P,rho_fluid)
-ylabel('P')
+ylabel('P [kbar]')
 xlabel('T')
-title('density fluid')
+title('density of fluid')
 shading interp
 colorbar
 
-%% Interpolate
-n = 100;
-P_int = linspace(0,2500,n)./1e3; %kbar
-T_int = linspace(0,300,n); % C
-x = linspace(0,10,n);
-
-% find hotter block
-ind = find(x<7.5 & x > 2.5);
-T_int(ind) = 800;
-
-T_int = T_int+273.15; % C -> K
-
-hold on
-figure(10)
-line(P_int,T_int,'LineWidth',4,'Color','r')
-figure(11)
-line(P_int,T_int,'LineWidth',4,'Color','r')
-figure(12)
-line(P_int,T_int,'LineWidth',4,'Color','r')
-hold off
-
-% interpolate along line
-rho_int         = interp2(P,T,rho,P_int,T_int);
-melt_int        = interp2(P,T,melt,P_int,T_int);
-rho_fluid_int   = interp2(P,T,rho_fluid,P_int,T_int);
-
-
-rho_int_final   = (melt_int.*rho_fluid_int) + ((1-melt_int).*rho_int);
-
-figure(20),clf
-
-subplot(2,1,1)
-plot(x,rho_int_final);
+subplot(224)
+rho_average = (1-melt).*rho + melt.*rho_fluid;
+pcolor(T,P,rho_average)
+ylabel('P [kbar]')
 xlabel('T')
-ylabel('density including melt')
-
-subplot(2,1,2)
-plot(x,melt_int)
-xlabel('T')
-ylabel('melt content')
-
-
-abc = 1;
-
-
-
-
-
-
-
+title('Combined density of fluid + solid (should NOT be passed to LaMEM!)')
+shading interp
+colorbar
 
 
 
