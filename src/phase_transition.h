e4788ed4a517e9a694c861a30f6c2d907cc49515
Jana.Schierjott <jana.schierjott@erdw.ethz.ch>
2021-04-01 08:51:33 -1000

    had to edit some functions here (there were too many inputs, maybe because of fetching???) and added the new fucntion Check_DikeBox_Phase_Transition() here as well.

--- src/phase_transition.h
+++ src/phase_transition.h
@@ -11,7 +11,8 @@ struct Ph_trans_t;
 struct JacRes;
 struct ConstEqCtx;
 struct Marker;
-
+struct Material_t;
+struct Freesurf;
 
 // read phase transition law
 PetscErrorCode DBMatReadPhaseTr(DBMat *dbm, FB *fb);
@@ -22,11 +23,14 @@ PetscErrorCode SetClapeyron_Eq(Ph_trans_t *ph);
 PetscErrorCode Overwrite_density(DBMat *dbm);
 PetscErrorCode Phase_Transition(AdvCtx *actx);
 PetscErrorCode Transition(Ph_trans_t *PhaseTrans, Marker *P, PetscInt PH1,PetscInt PH2, 
-                            Controls ctrl,Scaling *scal, SolVarCell *svCell, PetscInt *ph, PetscScalar *T, PetscInt *InsideAbove, PetscScalar);
+			  Controls ctrl,Scaling *scal, SolVarCell *svCell, PetscInt *ph, PetscScalar *T, JacRes *jr);  //, PetscInt *InsideAbove, PetscScalar); CHANGED BY JS
 PetscInt Check_Phase_above_below(PetscInt *phase_array, Marker *P,PetscInt num_phas);
-PetscInt Check_Constant_Phase_Transition(Ph_trans_t *PhaseTrans,Marker *P,PetscInt PH1, PetscInt PH2, Controls ctrl, SolVarCell *svCell, PetscInt *ph, PetscInt *InsideAbove, PetscScalar time);
-PetscInt Check_Box_Phase_Transition(Ph_trans_t *PhaseTrans,Marker *P,PetscInt PH1, PetscInt PH2, Scaling *scal, PetscInt *ph, PetscScalar *T, PetscInt *InsideAbove);      
-PetscInt Check_Clapeyron_Phase_Transition(Ph_trans_t *PhaseTrans,Marker *P,PetscInt PH1, PetscInt PH2, Controls ctrl, PetscInt *ph, PetscInt *InsideAbove); 
-PetscErrorCode Check_AirPhaseRatio_Box_Transition(Material_t *mat, ConstEqCtx *ctx, SolVarCell *svCell);  // NEW FOR DIKE, air check inside dike phase transition box, maybe ConstEqCtx *ctx not necessary??
+PetscInt Check_Constant_Phase_Transition(Ph_trans_t *PhaseTrans,Marker *P,PetscInt PH1, PetscInt PH2, Controls ctrl, SolVarCell *svCell); //, PetscInt *ph, PetscInt *InsideAbove, PetscScalar time);
+PetscInt Check_Box_Phase_Transition(Ph_trans_t *PhaseTrans,Marker *P,PetscInt PH1, PetscInt PH2, Scaling *scal, PetscInt *ph, PetscScalar *T); //, PetscInt *InsideAbove);
+PetscInt Check_DikeBox_Phase_Transition(Ph_trans_t *PhaseTrans,Marker *P,PetscInt PH1, PetscInt PH2, Scaling *scal, PetscInt *ph_out, PetscScalar *T_out, JacRes *jr);
+PetscInt Check_Clapeyron_Phase_Transition(Ph_trans_t *PhaseTrans,Marker *P,PetscInt PH1, PetscInt PH2, Controls ctrl);//, PetscInt *ph);//, PetscInt *InsideAbove); 
+
+// edited the Check_Constant_Phase_Transition() for *ctrl instead of ctrl, same for Check_Clapeyron_Phase_Transition()
+//PetscErrorCode Check_AirPhaseRatio_Box_Transition(Material_t *mat, Controls ctrl, SolVarCell *svCell);  // NEW FOR DIKE, air check inside dike phase transition box, maybe ConstEqCtx *ctx not necessary??
 
 #endif /* PHASE_TRANSITION_H_ */
