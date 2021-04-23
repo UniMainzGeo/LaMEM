9d1f6a6d2e6baf60e954679a0703fad7cd930270
Jana.Schierjott <jana.schierjott@erdw.ethz.ch>
2021-04-23 11:36:06 -1000

    removed the print statement

--- src/advect.cpp
+++ src/advect.cpp
@@ -831,11 +831,8 @@ PetscErrorCode ADVInterpFieldToMark(AdvCtx *actx, InterpCase icase)
 		  	phase_ID=P->phase;
 			mat = actx->dbm->phases + phase_ID;
 
-		  //if(mat->healTau){ P->APS /= (dt/mat->healTau+1);}
-
 			P->APS /= (dt/mat->healTau+1);
 
-		//PetscPrintf(PETSC_COMM_WORLD, " phase_ID=%d, healTau=%f, APS=%f  \n", 1, phase_ID, mat->healTau, P->APS);
 		}
 		else if(icase == _ATS_)
 		{
