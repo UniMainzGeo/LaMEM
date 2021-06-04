f9bc5548ed86821e9f884452a096a400d4141f89
Jana.Schierjott <jana.schierjott@erdw.ethz.ch>
2021-06-03 18:10:25 -1000

    added again the variable, is it necessary?

--- src/JacRes.h
+++ src/JacRes.h
@@ -86,6 +86,7 @@ struct SolVarBulk
 	PetscScalar  rho_pf; // fluid density from phase diagram
 	PetscScalar  mf;     // melt fraction from phase diagram
 	PetscScalar  phi;    // PSD angle
+	PetscScalar  dikeRHS; // additional term on RHS in divergence equation
 	PetscScalar  Ha ;    // Adiabatic heating
 
 };
