7213f45c92f83aa683b8259c53564b5c67469308
Boris Kaus <kaus@uni-mainz.de>
2023-01-26 03:41:24 +0100

    more graceful error messages
    Former-commit-id: a362c205cb080b5b61680b931ddc80504c70c146

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,4 +1,4 @@
-# These are tools that help perform the LaMEM tests run LaMEM locally
+# These are tools that help perform the LaMEM tests, which run LaMEM locally
 
 """
     run_lamem_local_test(ParamFile::String, cores::Int64=1, args::String=""; 
@@ -19,31 +19,42 @@ function run_lamem_local_test(ParamFile::String, cores::Int64=1, args::String=""
         exec=joinpath(cur_dir,bin_dir,"deb","LaMEM")
     end
 
-   
+    success = true
     if cores==1
         perform_run = `$(exec) -ParamFile $(ParamFile) $args`;
         
         # Run LaMEM on a single core, which does not require a working MPI
-        if !isempty(outfile)
-            run(pipeline(perform_run, stdout=outfile));
-        else
-            run(perform_run);
+        try 
+            if !isempty(outfile)
+                run(pipeline(perform_run, stdout=outfile));
+            else
+                run(perform_run);
+            end
+        catch
+            println("An error occured in directory: $(cur_dir) ")
+            println("while running the script:")
+            println(perform_run)
+            success = false
         end
     else
         perform_run = `$(mpiexec) -n $(cores) $(exec) -ParamFile $(ParamFile) $args`;
         # set correct environment
         #mpirun = setenv(mpiexec, LaMEM_jll.JLLWrappers.JLLWrappers.LIBPATH_env=>LaMEM_jll.LIBPATH[]);
-
         # Run LaMEM in parallel
-        if !isempty(outfile)
-            run(pipeline(perform_run, stdout=outfile));
-        else
-            run(perform_run);
+        try 
+            if !isempty(outfile)
+                run(pipeline(perform_run, stdout=outfile));
+            else
+                run(perform_run);
+            end
+        catch
+            println(perform_run)
+            success = false
         end
-
     end
+  
 
-    return nothing
+    return success
 end
 
 
@@ -80,11 +91,12 @@ end
 
 
 """
-    compare_logfiles(new::String, expected::String, 
+    success = compare_logfiles(new::String, expected::String, 
                         keywords::NTuple{N,String}=("|Div|_inf","|Div|_2","|mRes|_2"), 
                         accuracy=((rtol=1e-6,), (rtol=1e-6,), (rtol=1e-6,)))
 
-This compares two logfiles (different parameters which can be indicated) 
+This compares two logfiles (different parameters which can be indicated). If the length of the vectors is not the same, or the accuracy criteria are not met, success=false and info is displayed, to help track down the matter.
+
 """
 function compare_logfiles(new::String, expected::String, 
                         keywords::NTuple{N,String}=("|Div|_inf","|Div|_2","|mRes|_2"), 
@@ -94,9 +106,7 @@ function compare_logfiles(new::String, expected::String,
     exp_out = extract_info_logfiles(expected, keywords)
 
     test_status = true
-
     for i=1:N 
-
         rtol, atol = 0,0
         if  haskey(accuracy[i], :rtol)
             rtol = accuracy[i].rtol;
@@ -104,15 +114,21 @@ function compare_logfiles(new::String, expected::String,
         if  haskey(accuracy[i], :atol)
             atol = accuracy[i].atol;
         end
-
-        te =  isapprox(new_out[i], exp_out[i], rtol=rtol, atol=atol)
-        if te==false
+        if length(new_out[i])==length(exp_out[i])
+            te =  isapprox(new_out[i], exp_out[i], rtol=rtol, atol=atol)
+            if te==false
+                println("Problem with comparison of $(keywords[i]):")
+                print_differences( new_out[i], exp_out[i], accuracy[i])
+                test_status = false
+            end
+        else
             println("Problem with comparison of $(keywords[i]):")
-            print_differences( new_out[i], exp_out[i], accuracy[i])
+            println("length of vectors not the same")
             test_status = false
         end
-
+       
     end
+  
 
     return test_status
 end
@@ -148,11 +164,30 @@ function perform_lamem_test(dir::String, ParamFile::String, expectedFile::String
     else
         outfile = "test_$(cores).out";
     end
-    # perform simulation 
-    run_lamem_local_test(ParamFile, cores, args, outfile=outfile, bin_dir=bin_dir, opt=opt, deb=deb, mpiexec=mpiexec);
 
-    # compare logfiles 
-    success = compare_logfiles(outfile, expectedFile, keywords, accuracy)
+    # perform simulation 
+    success = run_lamem_local_test(ParamFile, cores, args, outfile=outfile, bin_dir=bin_dir, opt=opt, deb=deb, mpiexec=mpiexec);
+
+    if success
+        # compare logfiles 
+        success = compare_logfiles(outfile, expectedFile, keywords, accuracy)
+    end
+
+    if !success
+        # something went wrong with executing the file (likely @ PETSc error)
+        # Display some useful info here that helps debugging
+        println("Problem detected with test; see this on commandline with: ")
+        println("  dir=$(cur_dir) ")
+        println("  ParamFile=$(ParamFile) ")
+        println("  cores=$(cores) ")
+        println("  args=$(args) ")
+        println("  outfile=$(outfile) ")
+        println("  bindir=$(bin_dir) ")
+        println("  opt=$(opt) ")
+        println("  deb=$(deb) ")
+        println("  mpiexec=$(mpiexec) ")
+        println("  success = run_lamem_local_test(ParamFile, cores, args, outfile=nothing, bin_dir=bin_dir, opt=opt, deb=deb, mpiexec=mpiexec);")
+    end
 
     cd(cur_dir)  # return to directory       
     
