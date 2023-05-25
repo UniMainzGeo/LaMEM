7792abe60c2fe98a7f74934e67d25f78b83b3abc
Boris Kaus <kaus@uni-mainz.de>
2023-05-25 12:58:46 +0200

    add more dynamic libs

--- test/setup_packages.jl
+++ test/setup_packages.jl
@@ -12,14 +12,25 @@ petsc_dir = PETSc_jll.PATH_list[2][1:end-3]
 @show mpi_dir
 @show petsc_dir
 
-# print content of directories
 # copy mpi directories - we somehow have to do that one by one
 dirs = ["bin","lib","include","share"]
 for d in dirs
     run(`sudo -E cp -r $mpi_dir/$d /workspace/destdir/`)
 end
 
-run(`ls /workspace/destdir/`);
+# Same with petsc
+dirs = ["bin","lib","share"]
+for d in dirs
+    run(`sudo -E cp -r $mpi_dir/$d /workspace/destdir/`)
+end
+
+# And all required dynamic libraries (except petsc)
+for d in PETSc_jll.LIBPATH_list
+    if !contains(d, "julia") && !contains(d,"petsc")
+        run(`sudo -E cp -r $d /workspace/destdir/lib`)
+    end
+end
+
 
 # print
 run(`ls /workspace/destdir/lib`);
