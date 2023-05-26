ed4b28bbcf682b553f6d97f919c0d8a8dc269056
Boris Kaus <kaus@uni-mainz.de>
2023-05-26 18:29:02 +0200

    add test start file for CI

---
+++ test/start_tests_CI.jl
@@ -0,0 +1,10 @@
+# this starts the testing suite from the CI
+# the difference is that we use LaMEM compiled vs dynamic libraries so we need to add the environment
+using Pkg
+using PETSc_jll
+
+# run test suite
+Pkg.test("LaMEM_C", test_args=["use_dynamic_lib"])
+
+
+exit()
