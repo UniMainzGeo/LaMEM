e3d11cb93beb46e033c98f92e360ec5f3acf699f
Boris Kaus <kaus@uni-mainz.de>
2023-06-01 19:41:36 +0200

    deactivate superlu_dist

--- test/start_tests_CI.jl
+++ test/start_tests_CI.jl
@@ -10,6 +10,10 @@ else
     args_local = ["use_dynamic_lib","no_superlu"]
 end
 
+if "no_superlu" in ARGS
+    args_local = push!(args_local,"no_superlu")
+end
+
 # run test suite
 Pkg.test("LaMEM_C", test_args=args_local)
 
