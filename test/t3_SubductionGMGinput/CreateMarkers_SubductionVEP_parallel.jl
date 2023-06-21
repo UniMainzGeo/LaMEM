5fe7b6f829f62afd6a539db746bb10450388af49
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2023-06-21 16:27:25 +0200

    Update CreateMarkers_SubductionVEP_parallel.jl

--- test/t3_SubductionGMGinput/CreateMarkers_SubductionVEP_parallel.jl
+++ test/t3_SubductionGMGinput/CreateMarkers_SubductionVEP_parallel.jl
@@ -118,7 +118,7 @@ function CreateMarkers_SubductionVEP(dir="./", ParamFile="Subduction_VEP.dat"; N
     else
         #> 1 cores; create partitioning file first
         #PartFile = CreatePartitioningFile(ParamFile,NumberCores, LaMEM_dir="../../bin/opt/", verbose=false);
-        PartFile = CreatePartitioningFile_local(ParamFile, NumberCores; LaMEM_dir="../../bin/opt/", mpiexec=mpiexec)
+        PartFile = CreatePartitioningFile_local(ParamFile, NumberCores; LaMEM_dir="../../bin/", mpiexec=mpiexec)
         Save_LaMEMMarkersParallel(Model3D, PartitioningFile=PartFile,  directory="./markers", verbose=false, is64bit=is64bit)     
     end
 
