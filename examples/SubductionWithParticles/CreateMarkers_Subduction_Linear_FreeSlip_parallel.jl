13758ac79154bfa94d9c6201c2ddeed73ca63739
sgeotech <popov@uni-mainz.de>
2026-04-23 19:37:22 +0200

    Example problem generated with Julia
    Updated a couple of GMD function calls that were outdated.
    Julia script now accepts the partitioning file name as an argument
    (optional).
    Solver options in the input file are synchronized with the new version.
    Readme file describes the updated workflow.

--- examples/SubductionWithParticles/CreateMarkers_Subduction_Linear_FreeSlip_parallel.jl
+++ examples/SubductionWithParticles/CreateMarkers_Subduction_Linear_FreeSlip_parallel.jl
@@ -3,9 +3,13 @@
 # Load package that contains LaMEM I/O routines
 using GeophysicalModelGenerator  
 
+# get file names
+ParamFile = "Subduction2D_FreeSlip_Particles_Linear_DirectSolver.dat"
+
+
 # Load LaMEM particles grid
-ParamFile_2 =   "Subduction2D_FreeSlip_Particles_Linear_DirectSolver.dat"
-Grid        =   ReadLaMEM_InputFile(ParamFile_2)
+Grid = read_LaMEM_inputfile(ParamFile)
+
 
 # Specify slab parameters
 Trench_x_location   = -500;     # trench location
@@ -25,13 +29,13 @@ Phases      =   zeros(Int64, size(Grid.X));             # Rock numbers
 Temp        =   ones(Float64,size(Grid.X))*T_mantle;    # Temperature in C    
 
 # Create horizontal part of slab with crust & mantle lithosphere
-AddBox!(Phases,Temp,Grid,
+add_box!(Phases,Temp,Grid,
         xlim=(Trench_x_location, Trench_x_location+Length_Horiz_Slab), 
         zlim=(-ThicknessSlab   , 0.0),
         phase=LithosphericPhases(Layers=[ThicknessCrust ThicknessML], Phases=[1 2 0]) );               
 
 # Add inclined part of slab                            
-AddBox!(Phases,Temp,Grid,
+add_box!(Phases,Temp,Grid,
         xlim=(Trench_x_location-Length_Subduct_Slab, Trench_x_location), 
         zlim=(-ThicknessSlab   , 0.0),
         DipAngle=-SubductionAngle,
@@ -40,8 +44,13 @@ AddBox!(Phases,Temp,Grid,
 
 # Save julia setup 
 Model3D     =   CartData(Grid, (Phases=Phases,Temp=Temp))   # Create LaMEM model:
-Write_Paraview(Model3D,"LaMEM_ModelSetup")                  # Save model to paraview   (load with opening LaMEM_ModelSetup.vts in paraview)  
+write_paraview(Model3D,"LaMEM_ModelSetup")                  # Save model to paraview   (load with opening LaMEM_ModelSetup.vts in paraview)  
 
 # Save LaMEM markers
-Save_LaMEMMarkersParallel(Model3D)                          # Create LaMEM marker input on 1 core
+
+if size(ARGS)[1] == 1
+	save_LaMEM_markers_parallel(Model3D, PartitioningFile=ARGS[1])
+else
+	save_LaMEM_markers_parallel(Model3D)
+end
 
