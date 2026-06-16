d8a24e258258d9b6356042e3662195683eb85caf
sgeotech <popov@uni-mainz.de>
2026-06-16 11:42:00 +0200

    Automatic integer type modes in test framework
    Determine type of integer variables PETSc is using (32-bit vs. 64-bit)
    and set flags accordingly for the testing framework. Also check that
    both debug and optimized version use the same type of integers.

--- test/start_tests.jl
+++ test/start_tests.jl
@@ -1,5 +1,12 @@
 using Pkg
 
+if "is64bit" in ARGS
+    args_local = ["is64bit",]
+else
+    # 32bit PETSc installation
+    args_local = ["",]
+end
+
 # compile LaMEM if required
 cur_dir = pwd()
 cd("../src")
@@ -8,6 +15,6 @@ run(`make mode=deb all`)
 cd(cur_dir)
 
 # run test suite
-Pkg.test("LaMEM_C")
+Pkg.test("LaMEM_C", test_args=args_local)
 
 exit()
